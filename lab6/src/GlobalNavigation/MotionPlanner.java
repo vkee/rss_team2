93cc8642a0a329db13dd6bb673b5b7a8460ade8a
David Sukhin <dsukhin@mit.edu>
2015-03-09 17:10:13 -0400

    segment intersection code -dsukhin

--- lab6/src/GlobalNavigation/MotionPlanner.java
+++ lab6/src/GlobalNavigation/MotionPlanner.java
@@ -7,4 +7,20 @@ public class MotionPlanner {
     public MotionPlanner() {
         
     }
+    
+    public lineIntersects(PolygonObstacle p, Point2D.Double x1, Point2D.Double x2)
+    	{
+			Line2D path = new Line2D.Double(100, 100, 200, 200);
+			List<Point2D.Double> verts = p.getVertices();
+    	for (int i=0; i<verts.length; i++)
+    		{
+    		Point2D.Double point1 = verts.get(i);
+    		Point2D.Double point2 = verts.get((i+1)%verts.length);
+				Line2D side = new Line2D.Double(point1.x, point1.y, point2.x, point2.y);
+    		if (side.intersectsLine(path)) return true;
+    		}
+    	return false;
+    	}
+    
+    
 }
