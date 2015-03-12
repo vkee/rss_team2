7aa0f60c4bf39108ac25fbbad96cbcb5a46ac9b9
Vincent Kee <vkee@mit.edu>
2015-03-12 10:07:18 -0400

    added import for motion msg

--- lab6/src/GlobalNavigation/GlobalNavigation.java
+++ lab6/src/GlobalNavigation/GlobalNavigation.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import org.ros.message.rss_msgs.MotionMsg;
 
 import org.ros.node.topic.Publisher;
 import org.ros.node.topic.Subscriber;
