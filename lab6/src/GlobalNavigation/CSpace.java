0951e2b33d58ab304459597e279dfe22f6f2814d
Vincent Kee <vkee@mit.edu>
2015-03-13 12:10:12 -0400

    changed cspace robot size

--- lab6/src/GlobalNavigation/CSpace.java
+++ lab6/src/GlobalNavigation/CSpace.java
@@ -31,7 +31,7 @@ public class CSpace {
 		// Generating the robot polygon
 		PolygonObstacle origRobotPoly = new PolygonObstacle();
 		// manually defining robot polygon
-		double squareSideLength = .2;
+		double squareSideLength = .375;
 		origRobotPoly.addVertex(-squareSideLength, -squareSideLength);
 		origRobotPoly.addVertex(-squareSideLength, squareSideLength);
 		origRobotPoly.addVertex(squareSideLength, squareSideLength);
