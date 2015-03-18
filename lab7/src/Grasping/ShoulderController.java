1435943d2d5acc16596c6c54cd4fb768d920418c
rss-student <rss-student@ubuntu.mit.edu>
2015-03-17 20:59:39 -0400

    re adding

---
+++ lab7/src/Grasping/ShoulderController.java
@@ -0,0 +1,61 @@
+package Grasping;
+
+public class ShoulderController extends JointController {
+	private final double GYM_UP_ANGLE = Math.PI / 4;
+	private int GYM_UP_PWM;// hardcoded for now
+	protected final double GYM_GROUND_ANGLE = -Math.PI / 4;
+	protected final int GYM_GROUND_PWM = 525; // hardcoded for now
+
+	public ShoulderController(int minPWM, int maxPWM, double thetaRange,
+			int pwm0, int pwm270) {
+		super(minPWM, maxPWM, thetaRange, pwm0, pwm270);
+		GYM_UP_PWM = maxPWM;
+		// GYM_UP_PWM = getPWM(GYM_UP_ANGLE);
+		// GYM_GROUND_PWM = getPWM(GYM_GROUND_ANGLE);
+		// System.out.println("Gym Up PWM: " + GYM_UP_PWM);
+		// System.out.println("Gym Ground PWM: " + GYM_GROUND_PWM);
+	}
+
+	/**
+	 * Determines whether the shoulder joint is at the move up with a desired
+	 * angle state
+	 * 
+	 * @param currPWM
+	 *            the last PWM value written to the shoulder
+	 */
+	public boolean isGymUp(int currPWM) {
+		return (currPWM == GYM_UP_PWM);
+	}
+
+	/**
+	 * Returns the PWM value to write to the shoulder servo to move the arm up
+	 * 
+	 * @param currPWM
+	 *            the last PWM value written to the shoulder
+	 */
+	public int moveGymUp(int currPWM) {
+		return getSafePWM(currPWM, GYM_UP_PWM);
+	}
+
+	/**
+	 * Determines whether the shoulder joint is such that the arm is on the
+	 * ground
+	 * 
+	 * @param currPWM
+	 *            the last PWM value written to the shoulder
+	 */
+	public boolean onGround(int currPWM) {
+		return (currPWM == GYM_GROUND_PWM);
+	}
+
+	/**
+	 * Returns the PWM value to write to the shoulder servo to move the arm to
+	 * the ground
+	 * 
+	 * @param currPWM
+	 *            the last PWM value written to the shoulder
+	 */
+	public int moveToGround(int currPWM) {
+		return getSafePWM(currPWM, GYM_GROUND_PWM);
+	}
+}
---
+++ lab7/src/Grasping/WristController.java
@@ -0,0 +1,28 @@
+package Grasping;
+
+public class WristController extends JointController{
+    private final double GYM_BEND_ANGLE = Math.PI/2;
+    private final int GYM_BEND_PWM = 300; // hardcoded for now
+    
+    public WristController(int minPWM, int maxPWM, double thetaRange, int pwm0, int pwm90) {
+        super(minPWM, maxPWM, thetaRange, pwm0, pwm90);
+//        GYM_BEND_PWM = getPWM(GYM_BEND_ANGLE);
+        System.out.println("Gym Bend PWM: " + GYM_BEND_PWM);
+    }
+    
+    /**
+     * Determines whether the wrist joint is at the bend with a desired angle state
+     * @param currPWM the last PWM value written to the wrist
+     */
+    public boolean isGymBent(int currPWM) {
+        return (currPWM == GYM_BEND_PWM);
+    }
+    
+    /**
+     * Returns the PWM value to write to the wrist servo
+     * @param currPWM the last PWM value written to the wrist
+     */
+    public int bendGym(int currPWM) {
+        return getSafePWM(currPWM, GYM_BEND_PWM);
+    }   
+}
