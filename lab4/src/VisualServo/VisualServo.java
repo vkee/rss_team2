b14aec40deaa9c0e1764a75de4f03c0edb0241bd
Vincent Kee <vkee@mit.edu>
2015-03-18 03:53:29 -0400

    should give histogram now

--- lab4/src/VisualServo/VisualServo.java
+++ lab4/src/VisualServo/VisualServo.java
@@ -108,10 +108,10 @@ public class VisualServo implements NodeMain, Runnable {
 			// Begin Student Code
 
 			// publish velocity messages to move the robot towards the target
-			MotionMsg msg = new MotionMsg(); // (Solution)
-			msg.translationalVelocity = blobTrack.translationVelocityCommand; // (Solution)
-			msg.rotationalVelocity = blobTrack.rotationVelocityCommand; // (Solution)
-			publisher.publish(msg); // (Solution)
+//			MotionMsg msg = new MotionMsg(); // (Solution)
+//			msg.translationalVelocity = blobTrack.translationVelocityCommand; // (Solution)
+//			msg.rotationalVelocity = blobTrack.rotationVelocityCommand; // (Solution)
+//			publisher.publish(msg); // (Solution)
 
 			// End Student Code
 		}
