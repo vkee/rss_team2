3519c7c605c24f6dcdf53d05180cad790d0d3a5d
Vincent Kee <vkee@mit.edu>
2015-04-05 22:11:09 -0400

    3d cspace module complete

--- R2DTeam/MotionPlanning/CSpace.java
+++ R2DTeam/MotionPlanning/CSpace.java
@@ -14,41 +14,54 @@ import GlobalNavigation.PolygonObstacle;
  * configuration space of the environment.
  */
 public class CSpace {
-//    Robot Corner Coordinates wrt coordinate origin at the robot axis of rotation, where x is pointing forward,
-//    y is pointing left, and z is pointing up (when the robot is at 0 rad position in polar coordinates, x and y
-//    are aligned with the polar axes
-//    NOTE: These are not necessary the corners of the robot, but the outer dimension of the entire robot
-//    TODO: double check these measurements
+    //    Robot Corner Coordinates wrt coordinate origin at the robot axis of rotation, where x is pointing forward,
+    //    y is pointing left, and z is pointing up (when the robot is at 0 rad position in polar coordinates, x and y
+    //    are aligned with the polar axes)
+    //    NOTE: These are not necessary the corners of the robot, but the outer dimension of the entire robot
+    //    TODO: double check these measurements
     private final double ROBOT_TL_X = 0.190; // robot top left x dimension
     private final double ROBOT_TL_Y = 0.215; // robot top left y dimension
     private final double ROBOT_TR_X = 0.190; // robot top right x dimension
     private final double ROBOT_TR_Y = -0.215; // robot top right y dimension
-    private final double ROBOT_BL_X = -0.310; // robot bottom left x dimension
-    private final double ROBOT_BL_Y = 0.190; // robot bottom left y dimension
     private final double ROBOT_BR_X = -0.310; // robot bottom right x dimension
     private final double ROBOT_BR_Y = -0.190; // robot bottom right y dimension
+    private final double ROBOT_BL_X = -0.310; // robot bottom left x dimension
+    private final double ROBOT_BL_Y = 0.190; // robot bottom left y dimension
+
+    //    Number of angles to compute the configuration space for
+    protected final int NUM_ANGLES = 360;
+
+    //    Reflected robot polygons
+    private final ArrayList<PolygonObstacle> robotPolys = new ArrayList<PolygonObstacle>();
 
     public CSpace() {
-        // Generating the robot polygon
-        robotPoly = new PolygonObstacle();
-        // manually defining robot polygon
 
-        for (int i = 0; i < NUM_SIDES; i++) {
-            double theta = 2 * Math.PI / NUM_SIDES * i;
-            robotPoly.addVertex(ROBOT_SIDE_DIM * Math.cos(theta),
-                    ROBOT_SIDE_DIM * Math.sin(theta));
+        //        Constructing a robot polygon for each angle
+        for (int i = 0; i < NUM_ANGLES; i++) {
+            PolygonObstacle robotPoly = new PolygonObstacle();
+
+            double theta = 2* Math.PI / NUM_ANGLES * i;
+            robotPoly.addVertex(rotateReflectPt(ROBOT_TL_X, ROBOT_TL_Y, theta));
+            robotPoly.addVertex(rotateReflectPt(ROBOT_TR_X, ROBOT_TR_Y, theta));
+            robotPoly.addVertex(rotateReflectPt(ROBOT_BR_X, ROBOT_BR_Y, theta));
+            robotPoly.addVertex(rotateReflectPt(ROBOT_BL_X, ROBOT_BL_Y, theta));
+            robotPoly.close();
+
+            robotPolys.add(robotPoly);
         }
+    }
 
-        robotPoly.close();
-
-        // origRobotPoly.addVertex(0.0, 0.0);
-        // origRobotPoly.addVertex(ROBOT_WIDTH, 0.0);
-        // origRobotPoly.addVertex(ROBOT_WIDTH, ROBOT_HEIGHT);
-        // origRobotPoly.addVertex(0.0, ROBOT_HEIGHT);
-        // origRobotPoly.close();
-        // robotPoly = origRobotPoly;
-        // robotPoly = changeOrigin(origRobotPoly, new Point2D.Double(0.0,
-        // 0.0));
+    /**
+     * Rotates a point about the origin CCW using a rotation matrix 
+     * and then reflects it about the origin (negate x and y coordinates)
+     * See http://en.wikipedia.org/wiki/Rotation_matrix for rotation matrix
+     * @param x the x coordinate of the point
+     * @param y the y coordinate of the point
+     * @param theta the angle to rotate the point
+     * @return the new rotated point
+     */
+    private Point2D.Double rotateReflectPt(double x, double y, double theta) {
+        return new Point2D.Double(-(Math.cos(theta)*x - Math.sin(theta)*y), -(Math.sin(theta)*x + Math.cos(theta)*y));
     }
 
     /**
@@ -87,72 +100,71 @@ public class CSpace {
      *            the robot's reference point
      * @return the obstacle configuration space
      */
-    public PolygonObstacle obsCSpace(PolygonObstacle obsPoly,
-            PolygonObstacle robotPoly, Point2D.Double refPoint,
-            boolean computeRobotPoly) {
+    public PolygonObstacle obsCSpace(PolygonObstacle robotPoly, PolygonObstacle obsPoly) {
         return GeomUtils.convexHull(computeMSum(robotPoly, obsPoly)
                 .getVertices());
     }
 
     /**
-     * Computes the configuration space of the provided map.
+     * Computes the 3D configuration space of the provided map.
      * 
      * @param polyMap
      *            the map of the environment to generate a configuration space
      *            of
-     * @return the configuration space obstacles of the map obstacles and the
-     *         boundaries
+     * @return the 3D C-Space obstacles of the map obstacles and the
+     *         boundaries, where index of the returned list corresponds
+     *         to the C-Space obstacles generated for robot's orientation
+     *         at index degrees wrt 0
      */
-    public List<PolygonObstacle> envConfSpace(PolygonMap polyMap) {
-        List<PolygonObstacle> obsCSpaces = new ArrayList<PolygonObstacle>();
+    public ArrayList<ArrayList<PolygonObstacle>> generateCSpace(PolygonMap polyMap) {
+        ArrayList<ArrayList<PolygonObstacle>> obs3DCSpace = new ArrayList<ArrayList<PolygonObstacle>>();
 
-        // obsCSpaces.add(robotPoly);
+        //        Generating the 2D C-Space for each theta
+        for (int i=0; i < NUM_ANGLES; i++) {
+            ArrayList<PolygonObstacle> obs2DCSpace = new ArrayList<PolygonObstacle>();
+            
+            //            Computing the configuration spaces for each obstacle
+            for (PolygonObstacle obstacle : polyMap.getObstacles()) {
+                obs2DCSpace.add(obsCSpace(robotPolys.get(i), obstacle));
+            }
 
-        // Computed the configuration spaces of the obstacle
+            //            Adding an obstacle for the map boundaries
+            Rectangle2D.Double envBounds = polyMap.worldRect;
 
-        for (PolygonObstacle obstacle : polyMap.getObstacles()) {
-            obsCSpaces.add(obsCSpace(obstacle, robotPoly, null, false));
+            PolygonObstacle boundaryObs = new PolygonObstacle();
+
+            // bottom
+            boundaryObs.addVertex(envBounds.getX(), envBounds.getY());
+            boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
+                    envBounds.getY());
+            obs2DCSpace.add(obsCSpace(robotPolys.get(i), boundaryObs));
+
+            // right
+            boundaryObs = new PolygonObstacle();
+            boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
+                    envBounds.getY());
+            boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
+                    envBounds.getY() + envBounds.getHeight());
+            obs2DCSpace.add(obsCSpace(robotPolys.get(i), boundaryObs));
+
+            // left
+            boundaryObs = new PolygonObstacle();
+            boundaryObs.addVertex(envBounds.getX(), envBounds.getY());
+            boundaryObs.addVertex(envBounds.getX(),
+                    envBounds.getY() + envBounds.getHeight());
+            obs2DCSpace.add(obsCSpace(robotPolys.get(i), boundaryObs));
+
+            // top
+            boundaryObs = new PolygonObstacle();
+            boundaryObs.addVertex(envBounds.getX(),
+                    envBounds.getY() + envBounds.getHeight());
+            boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
+                    envBounds.getY() + envBounds.getHeight());
+            obs2DCSpace.add(obsCSpace(robotPolys.get(i), boundaryObs));
+            
+            obs3DCSpace.add(obs2DCSpace);
         }
-
-        // PolygonObstacle obstacle = polyMap.getObstacles().get(0);
-        // obsCSpaces.add(obsCSpace(obstacle, robotPoly, null, false));
-
-        // obsCSpaces.add(obsCSpace(obstacle, robotPoly, null, false));
-
-        // build obstacle for the boundaries
-        Rectangle2D.Double envBounds = polyMap.worldRect;
-
-        PolygonObstacle boundaryObs = new PolygonObstacle();
-
-        // bottom
-        boundaryObs.addVertex(envBounds.getX(), envBounds.getY());
-        boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
-                envBounds.getY());
-        obsCSpaces.add(obsCSpace(boundaryObs, robotPoly, null, false));
-
-        // right
-        boundaryObs = new PolygonObstacle();
-        boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
-                envBounds.getY());
-        boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
-                envBounds.getY() + envBounds.getHeight());
-        obsCSpaces.add(obsCSpace(boundaryObs, robotPoly, null, false));
-
-        // left
-        boundaryObs = new PolygonObstacle();
-        boundaryObs.addVertex(envBounds.getX(), envBounds.getY());
-        boundaryObs.addVertex(envBounds.getX(),
-                envBounds.getY() + envBounds.getHeight());
-        obsCSpaces.add(obsCSpace(boundaryObs, robotPoly, null, false));
-
-        // top
-        boundaryObs = new PolygonObstacle();
-        boundaryObs.addVertex(envBounds.getX(),
-                envBounds.getY() + envBounds.getHeight());
-        boundaryObs.addVertex(envBounds.getX() + envBounds.getWidth(),
-                envBounds.getY() + envBounds.getHeight());
-        obsCSpaces.add(obsCSpace(boundaryObs, robotPoly, null, false));
-
-        return obsCSpaces;
+        
+        return obs3DCSpace;
     }
 }
