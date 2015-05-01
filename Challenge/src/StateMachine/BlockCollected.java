54ab38c6b0190bf1c9ecb97c3c3a4423ce7236d8
hege <pghege@gmail.com>
2015-05-01 15:52:30 -0400

    missing curly brace

--- Challenge/src/StateMachine/BlockCollected.java
+++ Challenge/src/StateMachine/BlockCollected.java
@@ -8,53 +8,53 @@ import StateMachine.FSM.msgENUM;
 import StateMachine.FSM.stateENUM;
 
 /**
- * This state triggers when the block door opens because a block was collected. The state ends when the door releases
+ * This state triggers when the block door opens because a block was collected.
+ * The state ends when the door releases
  */
 public class BlockCollected implements FSMState {
-
 
 	private FSM fsm;
 	private WaypointNav waypointNavigator;
 
-	public BlockCollected(FSM stateMachine)
-		{
+	public BlockCollected(FSM stateMachine) {
 		fsm = stateMachine;
 		fsm.blocksCollected++;
-		//init any variables for this state
+		// init any variables for this state
 
 		ArrayList<Point2D.Double> waypoints = new ArrayList<Point2D.Double>();
 		waypoints.add(fsm.currentLocation);
-		waypointNavigator = new WaypointNav(waypoints, fsm.currentLocation, fsm);		//waypont nav to exact goal location
-		}	
+		waypointNavigator = new WaypointNav(waypoints, fsm.currentLocation, fsm); // waypont
+																					// nav
+																					// to
+																					// exact
+																					// goal
+																					// location
+	}
 
-	
-	public stateENUM getName()
-		{return stateENUM.BLOCKCOLLECTED;}
+	public stateENUM getName() {
+		return stateENUM.BLOCKCOLLECTED;
+	}
 
-	
-	public boolean accepts(msgENUM msgType)
-		{
-		if (msgType == msgENUM.FLAP) return true;
+	public boolean accepts(msgENUM msgType) {
+		if (msgType == msgENUM.FLAP)
+			return true;
 		return false;
-		}
+	}
 
-
-	public void update(GenericMessage msg)
-		{
+	public void update(GenericMessage msg) {
 
 		waypointNavigator.wayptNav(message.x, message.y, message.theta);
 
 		if (waypointNavigator.isDone()) {
 			fsm.updateState(new NeckScan(fsm));
-
 		}
 
+	}
 
 	@Override
 	public void onStart() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
