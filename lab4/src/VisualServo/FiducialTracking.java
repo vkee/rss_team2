b0f38683f3a2dfef37d636fe95c7b4e61a64d362
hege <pghege@gmail.com>
2015-05-03 17:50:25 -0400

    adjust parameters

--- lab4/src/VisualServo/FiducialTracking.java
+++ lab4/src/VisualServo/FiducialTracking.java
@@ -151,7 +151,7 @@ public class FiducialTracking extends BlobTracking {
 					if (green < 0)
 						green += 255;
 
-					double ratio = 1.1;
+					double ratio = 1.3;
 					if (i == 0 && red >= ratio * blue && red >= ratio * green) {
 						mask[i][maskIndex++] = 255; // (Solution)
 					} else if (i == 1 && green >= ratio * red
