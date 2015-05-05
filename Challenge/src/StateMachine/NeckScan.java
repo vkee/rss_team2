04ae3fc0d1aa7c36b0d87a48898fa6c8d5d846a5
rss-student <rss-student@ubuntu.mit.edu>
2015-05-04 18:53:59 -0700

    laptop state

--- Challenge/src/StateMachine/NeckScan.java
+++ Challenge/src/StateMachine/NeckScan.java
@@ -82,7 +82,7 @@ public class NeckScan implements FSMState {
 			depth_array = cl.getDepthImage();
 			Image dest = new Image(src);
 			blobTrack.apply(src, dest, depth_array);
-	        fsm.vidPub.publish(dest);
+//	        fsm.vidPub.publish(dest);
 			detectedFids.addAll(blobTrack.getFiducials(src, depth_array));
 			try{Thread.sleep(2000);}catch(Exception e){}
 		}
