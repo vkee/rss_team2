b4b1433ead64ce3a75ab3454f3ae9afb1d86371d
Vincent Kee <vkee@mit.edu>
2015-05-05 18:57:46 -0400

    ready for particle filter updates

--- Challenge/src/StateMachine/NeckScan.java
+++ Challenge/src/StateMachine/NeckScan.java
@@ -28,111 +28,136 @@ import org.ros.message.Challenge_msgs.*;
  */
 public class NeckScan implements FSMState {
 
-	// TODO: make servos rotate
+    // TODO: make servos rotate
 
-	private FSM fsm;
-	private double neckAngleTarget = 0;
-	private final int NECKSTATES = 12;
-	protected FiducialTracking blobTrack = null;
-	client cl = null;
+    private FSM fsm;
+    private double neckAngleTarget = 0;
+    private final int NECKSTATES = 12;
+    protected FiducialTracking blobTrack = null;
+    client cl = null;
 
-	int debug_count;
+    private ArrayList<Point2D.Double> newGoals;
 
-	private ArrayList<Point2D.Double> newGoals;
+    public NeckScan(FSM stateMachine) {
+        fsm = stateMachine;
+        newGoals = new ArrayList<Point2D.Double>();
+        blobTrack = new FiducialTracking();
+        cl = new client();
 
-	public NeckScan(FSM stateMachine) {
-		fsm = stateMachine;
-		newGoals = new ArrayList<Point2D.Double>();
-		blobTrack = new FiducialTracking();
-		cl = new client();
+        // init any variables for this state
+    }
 
-		// init any variables for this state
-		debug_count = 0;
-	}
+    public stateENUM getName() {
+        return stateENUM.SCAN;
+    }
 
-	public stateENUM getName() {
-		return stateENUM.SCAN;
-	}
+    public boolean accepts(msgENUM msgType) {
+        if (msgType == msgENUM.SERVO)
+            return true;
+        return false;
+    }
 
-	public boolean accepts(msgENUM msgType) {
-		if (msgType == msgENUM.SERVO)
-			return true;
-		return false;
-	}
+    public void update(GenericMessage msg) {
+        Image src = null;
+        float[] depth_array = null;
+        List<FiducialObject> detectedFids = new ArrayList<FiducialObject>();
 
-	public void update(GenericMessage msg) {
-		Image src = null;
-		float[] depth_array = null;
-		List<FiducialObject> detectedFids = new ArrayList<FiducialObject>();
+        //		Making the Scans
 
-//		Making the 
-		
-		for (int i = 0; i < 3; i++) {
-			fsm.neckServo.goToSettingOne(i);
-			src = cl.getImage();
-			depth_array = cl.getDepthImage();
-			Image dest = new Image(src);
-			blobTrack.apply(src, dest, depth_array);
-			org.ros.message.sensor_msgs.Image pubImage = new org.ros.message.sensor_msgs.Image();
-			Image.fillImageMsg(pubImage, dest);
-			fsm.vidPub.publish(pubImage);
-			detectedFids.addAll(blobTrack.getFiducials(src, depth_array));
-			try {
-				Thread.sleep(3000);
-			} catch (Exception e) {
-			}
-		}
+        //		Forward
+        for (int i = 0; i < 3; i++) {
+            fsm.neckServo.goToSettingOne(i);
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+            }
+            src = cl.getImage();
+            depth_array = cl.getDepthImage();
+            Image dest = new Image(src);
+            blobTrack.apply(src, dest, depth_array);
+            org.ros.message.sensor_msgs.Image pubImage = new org.ros.message.sensor_msgs.Image();
+            Image.fillImageMsg(pubImage, dest);
+            fsm.vidPub.publish(pubImage);
+            detectedFids.addAll(blobTrack.getFiducials(src, depth_array));
+            //			try {
+            //				Thread.sleep(3000);
+            //			} catch (Exception e) {
+            //			}
+        }
 
-		for (int i = 3; i >= 0; i--) {
-			fsm.neckServo.goToSettingOne(i);
-			src = cl.getImage();
-			depth_array = cl.getDepthImage();
-			Image dest = new Image(src);
-			blobTrack.apply(src, dest, depth_array);
-			org.ros.message.sensor_msgs.Image pubImage = new org.ros.message.sensor_msgs.Image();
-			Image.fillImageMsg(pubImage, dest);
-			fsm.vidPub.publish(pubImage);
-			detectedFids.addAll(blobTrack.getFiducials(src, depth_array));
-			try {
-				Thread.sleep(3000);
-			} catch (Exception e) {
-			}
-		}
+        //		Backward
+        for (int i = 3; i >= 0; i--) {
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+            }
+            fsm.neckServo.goToSettingOne(i);
+            src = cl.getImage();
+            depth_array = cl.getDepthImage();
+            Image dest = new Image(src);
+            blobTrack.apply(src, dest, depth_array);
+            org.ros.message.sensor_msgs.Image pubImage = new org.ros.message.sensor_msgs.Image();
+            Image.fillImageMsg(pubImage, dest);
+            fsm.vidPub.publish(pubImage);
+            detectedFids.addAll(blobTrack.getFiducials(src, depth_array));
+            //			try {
+            //				Thread.sleep(3000);
+            //			} catch (Exception e) {
+            //			}
+        }
 
-		System.out.println(debug_count);
-		debug_count++;
-		ArrayList<Integer> measuredFids = new ArrayList<Integer>();
+        if (detectedFids.size() == 0) {
+            System.out.println("No Fids Detected");
+        }
 
-		for (FiducialObject fids : detectedFids) {
-			System.out.println("Distance to " + fids.getFiducialNumber()
-					+ " is " + fids.getDistanceTo() + " m");
-			measuredFids.add(fids.getFiducialNumber());
-		}
+        HashMap<Integer, Double> totalFidDist = new HashMap<Integer, Double>();
+        HashMap<Integer, Integer> numMeasurements = new HashMap<Integer, Integer>();
 
-		if (detectedFids.size() == 0) {
-			System.out.println("No Fids Detected");
-		}
-		// fsm.neckServo.fullCycle(false); // go back
+        //        Computing the number of measurements and distances
+        for (FiducialObject fids : detectedFids) {
+            int fidIndex = fids.getFiducialNumber();
+            double fidDist = fids.getDistanceTo();
+            //            Updating the distances
+            if (totalFidDist.containsKey(fidIndex)){
+                totalFidDist.put(fidIndex, totalFidDist.get(fidIndex) + fidDist);
+            } else {
+                totalFidDist.put(fidIndex, fidDist);
+            }
 
-		// // Determining the distances to the
-		// // fiducials that are in the FOV of the robot
-//		 HashMap<Integer, java.lang.Double> measuredDists = fsm.particleFilter.getFidsDists(fsm.prevPt, fsm.map, measuredFids);
-//		 fsm.particleFilter.measurementUpdate(measuredFids, measuredDists);
-//		
-//		 RobotParticle particle = fsm.particleFilter.sampleParticle();
-//		
-//		 System.out.println("Prev Stored Pt: X-" + fsm.prevPt.x + " Y-" +
-//		 fsm.prevPt.y);
-//		 System.out.println("Sampled Particle Position: X-" + particle.getX()
-//		 + " Y-" + particle.getY());
-//
-//		 fsm.updateODO(particle.getX() - fsm.CAMERA_X_POS*Math.cos(fsm.robotTheta), particle.getY() - fsm.CAMERA_X_POS*Math.sin(fsm.robotTheta));
-		//
-		 fsm.updateState(new WaypointNavClose(fsm));
-	}
+            //            Updating the number of measurements
+            if (numMeasurements.containsKey(fidIndex)){
+                numMeasurements.put(fidIndex, numMeasurements.get(fidIndex) + 1);
+            } else {
+                numMeasurements.put(fidIndex, 1);
+            }
+        }
 
-	@Override
-	public void onStart() {
-		// TODO Auto-generated method stub
-	}
+        ArrayList<Integer> measuredFids = new ArrayList<Integer>();
+        HashMap<Integer, Double> measuredDists = new HashMap<Integer, Double>();
+
+        //        Averaging the distances
+        for (Integer index : totalFidDist.keySet()) {
+            double avgDist = totalFidDist.get(index)/numMeasurements.get(index);
+            measuredDists.put(index, avgDist);
+        }
+
+        // Determining the distances to the fiducials that are in the FOV of the robot
+        fsm.particleFilter.measurementUpdate(measuredFids, measuredDists);
+
+        RobotParticle particle = fsm.particleFilter.sampleParticle();
+
+        System.out.println("Prev Stored Pt: X-" + fsm.prevPt.x + " Y-" +
+                fsm.prevPt.y);
+        System.out.println("Sampled Particle Position: X-" + particle.getX()
+                + " Y-" + particle.getY());
+
+        fsm.updateODO(particle.getX() - fsm.CAMERA_X_POS*Math.cos(fsm.robotTheta), particle.getY() - fsm.CAMERA_X_POS*Math.sin(fsm.robotTheta));
+
+        fsm.updateState(new WaypointNavClose(fsm));
+    }
+
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+    }
 }
