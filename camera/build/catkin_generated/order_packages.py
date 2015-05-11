f31cd28bcd0bf8c24341dc2a0f21eb5ca84ff077
rss-student <rss-student@ubuntu.mit.edu>
2015-05-11 12:30:51 -0700

    added camera stuff

---
+++ camera/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/rss-student/camera/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/rss-student/camera/devel;/opt/ros/indigo".split(';') if "/home/rss-student/camera/devel;/opt/ros/indigo" != "" else []
---
+++ camera/src/kinect/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/rss-student/camera/src/kinect/build/test_results"
+export CMAKE_PREFIX_PATH="/home/rss-student/camera/src/kinect/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/rss-student/camera/src/kinect/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/rss-student/camera/src/kinect/build/devel/lib:/home/rss-student/camera/src/kinect/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/rss-student/camera/src/kinect/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/rss-student/camera/src/kinect/build/devel/lib/pkgconfig:/home/rss-student/camera/src/kinect/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/rss-student/camera/src/kinect/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rss-student/camera/src/kinect/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/rss-student/camera/src/kinect:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/rss-student/camera/src/kinect/build/test_results"
